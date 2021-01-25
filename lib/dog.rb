50e2ef0b500ec6a661d84491fccd18b2f98be4e0
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-24 22:24:43 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+    attr_accessor :owner, :mood
+    attr_reader :name
+  
+    @@all = []
+    def initialize(name, owner)
+      @@all << self
+      @mood = "nervous"
+      @name = name
+      @owner = owner
+    end
+  
+    def self.all
+      @@all
+    end
+  
 end
